0059078b6aaa8e4dbb595bf3c1f745a7b77a217e
Yuri-debug <yurai.pustovoy@gmail.com>
2022-09-09 13:44:56 +0300

    added logic to currency component

--- src/modules/Currencies/components/Currency/Currency.tsx
+++ src/modules/Currencies/components/Currency/Currency.tsx
@@ -1,3 +1,85 @@
+import { ReactNode } from 'react'
+import { useSelector } from 'react-redux'
+import { selectCurrency } from '../../../../store/modules'
+import {
+  CurrencyLogoEuro,
+  CurrencyLogoUah,
+  CurrencyLogoUsd,
+  CurrencyText,
+  CurrencyWrapper,
+  ExchangeIcon,
+  Wrapper,
+} from './styled'
+
 export const Currency = () => {
-  return <></>
+  const currencies = [
+    {
+      id: 1,
+      base: 'uah',
+      logo: <CurrencyLogoEuro />,
+      code: 'EUR',
+      value: useSelector(selectCurrency).currency.EUR,
+    },
+    {
+      id: 2,
+      base: 'uah',
+      logo: <CurrencyLogoUsd />,
+      code: 'USD',
+      value: useSelector(selectCurrency).currency.USD,
+    },
+    {
+      id: 3,
+      base: 'eur',
+      logo: <CurrencyLogoUsd />,
+      code: 'USD',
+      value: (
+        useSelector(selectCurrency).currency.USD /
+        useSelector(selectCurrency).currency.EUR
+      ).toFixed(2),
+    },
+    {
+      id: 4,
+      base: 'eur',
+      logo: <CurrencyLogoUah />,
+      code: 'UAH',
+      value: (1 / useSelector(selectCurrency).currency.EUR).toFixed(3),
+    },
+    {
+      id: 5,
+      base: 'usd',
+      logo: <CurrencyLogoEuro />,
+      code: 'EUR',
+      value: (
+        useSelector(selectCurrency).currency.EUR /
+        useSelector(selectCurrency).currency.USD
+      ).toFixed(2),
+    },
+    {
+      id: 6,
+      base: 'usd',
+      logo: <CurrencyLogoUah />,
+      code: 'UAH',
+      value: (1 / useSelector(selectCurrency).currency.USD).toFixed(3),
+    },
+  ]
+
+  const baseCurrency = useSelector(selectCurrency).baseCurrency
+  return (
+    <Wrapper>
+      {currencies.map((currency): ReactNode => {
+        if (currency.base === baseCurrency) {
+          return (
+            <CurrencyWrapper key={currency.id}>
+              {currency.logo}
+              <CurrencyText>1 {currency.code}</CurrencyText>
+              <ExchangeIcon />
+              <CurrencyText>
+                {currency.value + ` ${currency.base.toUpperCase()}`}
+              </CurrencyText>
+            </CurrencyWrapper>
+          )
+        }
+      })}
+    </Wrapper>
+  )
 }
--- src/modules/Currencies/components/Currency/styled.ts
+++ src/modules/Currencies/components/Currency/styled.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { IoLogoUsd, IoLogoEuro } from 'react-icons/io'
+import { FaExchangeAlt, FaHryvnia } from 'react-icons/fa'
 
 export const Wrapper = styled.div`
   max-width: 30%:
@@ -7,4 +9,62 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`
+
+export const CurrencyWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+
+  margin: 20px 0;
+`
+
+export const CurrencyLogoEuro = styled(IoLogoEuro)`
+  width: 40px;
+  height: 40px;
+
+  color: #ffffff;
+
+  align-self: center;
+`
+
+export const CurrencyLogoUsd = styled(IoLogoUsd)`
+  width: 40px;
+  height: 40px;
+
+  color: #ffffff;
+
+  align-self: center;
+`
+
+export const CurrencyLogoUah = styled(FaHryvnia)`
+  width: 40px;
+  height: 40px;
+
+  color: #ffffff;
+
+  align-self: center;
+`
+
+export const CurrencyText = styled.p`
+  font-family: 'Poppins';
+  font-size: 2rem;
+
+  color: #ffffff;
+
+  align-self: center;
+`
+
+export const ExchangeIcon = styled(FaExchangeAlt)`
+  width: 40px;
+  height: 40px;
+
+  color: #ffffff;
+
+  align-self: center;
+
+  &:hover {
+    transition: 0.3s;
+
+    color: #f3666e;
+  }
 `
